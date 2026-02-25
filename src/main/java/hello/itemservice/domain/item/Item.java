4ed239c259ef6c6a04f9d144e9935b94ffb01e52
dhson <saozzang@gmail.com>
2026-02-25 23:07:41 +0900

    [#5-1]프로젝트준비

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,7 +1,11 @@
 package hello.itemservice.domain.item;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
@@ -9,8 +13,16 @@ import java.util.List;
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     private Boolean open; // 판매 여부
