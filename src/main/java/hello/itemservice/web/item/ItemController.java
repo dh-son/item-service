d375112cb2e56aabb6b1d3c9fb0ace2a5d2e6529
dhson <saozzang@gmail.com>
2026-03-03 22:55:41 +0900

    [#6-2]회원가입

--- src/main/java/hello/itemservice/web/item/ItemController.java
+++ src/main/java/hello/itemservice/web/item/ItemController.java
@@ -139,15 +139,4 @@ public class ItemController {
         itemRepository.update(itemId, itemParam);
         return "redirect:/items/{itemId}";
     }
-
-
-
-    /**
-     * 테스트용 데이터 추가
-     */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
-    }
 }
